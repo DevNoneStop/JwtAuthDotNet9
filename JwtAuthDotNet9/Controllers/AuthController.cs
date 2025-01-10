54b987298ebcb35be7b675bfa6cce4f8ba71abc1
Lesego Gaompe <lesego.gaompe@mukuru.com>
2025-01-10 10:49:59 +0200

    Created UserLogin with passwordHasher

--- JwtAuthDotNet9/Controllers/AuthController.cs
+++ JwtAuthDotNet9/Controllers/AuthController.cs
@@ -22,5 +22,21 @@ namespace JwtAuthDotNet9.Controllers
 
             return Ok(user);
         }
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not found");
+            }
+            if(new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password)
+                == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong password.");
+            }
+            string token = "success";
+            return Ok(token);
+        }
+
     }
 }
