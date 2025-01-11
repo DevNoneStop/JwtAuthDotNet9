22a3e78e40deba375c8c610e33f9def5ac604ae2
Lesego Gaompe <lesego.gaompe@mukuru.com>
2025-01-11 07:43:23 +0200

    Created a AppDbContext class and Added migration

---
+++ JwtAuthDotNet9/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using JwtAuthDotNet9.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthDotNet9.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
